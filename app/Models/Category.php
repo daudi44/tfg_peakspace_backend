4cee0f34d7b1610f7358d27fec325f4f4a584c82
Daniel Audí Bielsa <daniaudibielsa@gmail.com>
2025-04-26 18:46:33 +0200

    category mistake fixed

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -6,5 +6,45 @@ use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
 {
-    //
+    protected $fillable = [
+        'name',
+        'type',
+        'user_id',
+        'parent_category_id',
+    ];
+
+    public function parent()
+    {
+        return $this->belongsTo(Category::class, 'parent_category_id');
+    }
+
+    public function children()
+    {
+        return $this->hasMany(Category::class, 'parent_category_id');
+    }
+
+    public function movements()
+    {
+        return $this->hasMany(Movement::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function subscriptions()
+    {
+        return $this->hasMany(Subscription::class);
+    }
+
+    public function tasks()
+    {
+        return $this->hasMany(Task::class);
+    }
+
+    public function timeEntries()
+    {
+        return $this->hasMany(TimeEntry::class);
+    }
 }
